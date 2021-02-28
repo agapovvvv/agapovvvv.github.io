7282f00f2e8104a1ff90637b1ab83fcbbb8120c8
agapovvvv <65828933+agapovvvv@users.noreply.github.com>
2021-02-28 15:46:05 +0300

    Delete Scripts directory

--- Scripts/index.js
+++
@@ -1,8 +0,0 @@
-const mouse = document.getElementsByClassName('mouse')
-setInterval(() => {
-    if (pageYOffset > 300){
-        mouse[0].style.opacity = '0'
-    }else{
-        mouse[0].style.opacity = '1'
-    }
-}, 0);
