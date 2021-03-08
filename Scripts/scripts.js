6632cea06fd470060d919b4c03120a5776de08b6
agapovvvv <65828933+agapovvvv@users.noreply.github.com>
2021-03-08 19:33:23 +0300

    Delete Scripts directory

--- Scripts/scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.burger-container').click(function() {
-        $('.link-burger').toggleClass('open-menu');
-        $('.menu').toggleClass('open');
-    });
-});
